190663f20929fcafc1e16bc40ba2bbc1223552f4
a <ruan65@gmail.com>
2019-03-03 16:29:56 +0300

    mixins21

---
+++ lib/mixin/mixin21.dart
@@ -0,0 +1,24 @@
+abstract class Super {
+  void method() {
+    print("Super");
+  }
+}
+
+class MySuper implements Super {
+  void method() {
+    print("MySuper");
+  }
+}
+
+mixin Mixin on Super {
+  void method() {
+    super.method();
+    print("Sub");
+  }
+}
+
+class Client extends MySuper with Mixin {}
+
+void main() {
+  Client().method();
+}
